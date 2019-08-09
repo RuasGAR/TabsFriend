1e0d3cf36788d812af21bf63e187c153ef9dbe73
RuasGAR <gabrielamaralruas@gmail.com>
2019-08-09 14:15:17 -0300

    #import resolvido

--- Ionic/src/app/app.module.ts
+++ Ionic/src/app/app.module.ts
@@ -8,11 +8,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrepostComponent } from './componentes/prepost/prepost.component';
+
 
 
 @NgModule({
-  declarations: [AppComponent,PrepostComponent],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
