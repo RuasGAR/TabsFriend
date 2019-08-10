eb9e4b34d6404bf8c8fb26e6f07bba0e0afe3b43
renatojoao <joaorenato286@gmail.com>
2019-08-09 23:05:48 -0400

    + CORS

---
+++ Laravel/app/Http/Middleware/CORS.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CORS
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $answer = $next($request);
+        $answer->header('Access-Control-Allow-Origin','http://localhost:8100/')->header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS')->header('Access-Control-Allow-Headers','Authorization,Content-Type');
+        return $answer;
+    }
+}
